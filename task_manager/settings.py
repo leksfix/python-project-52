5512108b49798e3be19c0e8e3d273a3571f4428d
alex <leksfix@gmail.com>
2025-10-27 20:12:30 +0300

    Add psycopg2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "django-insecure-d%8(=jf9e#4zh&$q%=eoveuyj29x+8e4#%-ls(rof)+-cy^y%k"
+    "SECRET_KEY", "SOME_SECRET_KEY"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
