fc90173d3a81a7bf622b03648a824cb02b9f6ba9
alex <leksfix@gmail.com>
2025-11-01 15:55:29 +0300

    Added STATIC_ROOT

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -137,6 +137,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.getenv('STATIC_ROOT', BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
